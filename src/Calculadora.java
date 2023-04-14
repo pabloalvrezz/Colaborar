07ae18b1b0b46e13ffd97cd20dcf73d5266efc7b
nelaba23 <nelaba23@educastur.es>
2023-04-14 16:21:11 +0200

    Version 1.1: Metodo suma añadido

--- src/Calculadora.java
+++ src/Calculadora.java
@@ -2,27 +2,9 @@
 public class Calculadora 
 {
 	private float resultado; //Variable en la que se almacenara el resultado de las operaciones
-	private float num1,num2; //Variables que usaremos para almacenar los numeros introducidos por el usuario
+	private float num1, num2; //Variables que usaremos para almacenar los numeros introducidos por el usuario
 
-	//Getter para obtener el resultado
-	public float getResultado() 
-	{
-		return resultado;
-	}
-
-	//Getter para obtener el primer valor
-	public float getNum1()
-	{
-		return num1;
-	}
-	
-	//Getter para obtener el segundo valor
-	public float getNum2()
-	{
-		return num2;
-	}
-	
-	//Metodo que se llamara en el naim, sacara un mensaje por pantalla
+	//Metodo que se llamara en el main, sacara un mensaje por pantalla
 	//y cambiara el valor de num1 y num2 
     public void introducirDatos() 
     {
@@ -33,15 +15,17 @@ public class Calculadora
     //de la operacion de los dos numeros
     public void mostrarDatos() 
     {
-        
+
     }
 
-    //Metodo para sumar, almacenara el resultado en la variable resultado
-    //los numeros usados seran num1 y num2. Se llamara al metodo mostrarDatos
+    /**
+     * Metodo para sumar, almacenara el resultado en la variable resultado
+     * los numeros usados seran num1 y num2. Se llamara al metodo mostrarDatos
+     */
 	public void sumar() 
 	{
-		
-		
+		resultado = num1 + num2;
+		mostrarDatos();
 	}
 
 	//Metodo para restar, almacenara el resultado en la variable resultado
